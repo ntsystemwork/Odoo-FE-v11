747fb4ddcec4228aa2e99a8333243ce815b0c8b1
Wilfredo Moreno <wilfredomorenog@gmail.com>
2018-09-23 17:24:23 -0500

    Eliminados m´odulos

--- base_ux/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import ir_actions_act_window
-from . import res_partner
